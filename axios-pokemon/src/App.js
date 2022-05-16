32f0e2fa8c24aa266871ce000600745dc8366d7c
sescarrega <s.jescarrega@gmail.com>
2022-05-16 13:58:48 -0700

    axios and react iterating thru an array

--- axios-pokemon/src/App.js
+++ axios-pokemon/src/App.js
@@ -1,9 +1,56 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const App = () => {
+  const [pokemons, setPokemons]= useState([])
+
+  const pokemonFetch = ()=> {
+    // we can use full crud with this 
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0") 
+    // add promise res
+    .then(res => {
+      console.log(res.data.results);
+      setPokemons(res.data.results);
+    })
+    .catch(err => console.log(err))
+    // never omit .catch
+  }  
+    // how is pokemon fetch and pokemon  related ? 
   return (
-    <div>App</div>
+    <div> 
+      <h1>
+        Pokemon
+      </h1>
+      <button onClick={pokemonFetch}>Show Pokemon</button>
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <th>
+                {/* pokemon image */}
+              </th>
+              <th>
+                pokemon name
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+              {/*  iterate thru pokemon array to display names and images */}
+
+            {pokemons?  pokemons.map((pokemon, idx)=>  // can call elements whatever you want, use sngle to present elemnt 
+
+                        <tr key={idx}><td> {pokemon.name} </td></tr>
+
+            ): <tr><td> loading</td></tr>}  
+            {/*  not showing loading  */}
+          </tbody>
+            {/* <td> {pokemon.sprites.front_default}</td> */}
+        </table>
+      </div>
+
+
+
+    </div>
   )
 }
 
