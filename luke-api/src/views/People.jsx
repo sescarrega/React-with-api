813bf56412892479e970a654ae50d8bee321e4af
sescarrega <s.jescarrega@gmail.com>
2022-05-18 17:55:31 -0700

    react app

--- luke-api/src/views/People.jsx
+++ luke-api/src/views/People.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+// id is from routes 
+// import params
+//destructure params 
+
+// automatically load info from api via useEffect+ useStates
+// from api we need axios
 
 const People = () => {
-  return (
-    <div> hello from People</div>
-  )
+    const {id} = useParams()
+    const [people, setPeople] = useState()
+
+    useEffect(()=>{
+        axios.get(`https://swapi.dev/api/people/${id}/`)
+        .then(response => {
+            console.log(response.data)
+            setPeople(response.data)
+        })
+        .catch(err => console.log(err))
+    },[])
+
+
+    return (
+        <div> 
+            <p> hello from  people.jsx!!</p>
+            {
+                people?
+                <div>
+                    <h3> Name: {people.name}</h3>
+                    <p> id: {id}</p>
+                    <p> Height: {people.height}</p>
+                    <p> Mass: {people.mass}</p>
+
+                </div>:
+
+                <p> this person is not available </p>
+            }
+        </div>
+    )
 }
 
 export default People
--- luke-api/src/views/Planets.jsx
+++ luke-api/src/views/Planets.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const Planets = () => {
-  return (
-    <div> hello from Planets</div>
-  )
+    const {id} = useParams()
+    const [ planet, setPlanet] = useState()
+
+    useEffect(() => {
+        axios.get(`https://swapi.dev/api/planets/${id}/`)
+        .then(response => {
+            console.log(response.data)
+            setPlanet(response.data)
+        })
+        .catch(err => console.log(err))
+    }, [])
+
+    return (
+        <div> 
+            <h1> Planets</h1>
+            {
+                planet?
+                <div> 
+                    <h3> Name: {planet.name}</h3>
+                    <p> id: {id}</p>
+                    <p> Climate:{planet.climate} </p> 
+                    <p> Terrain:{planet.terrain} </p> 
+                </div> :
+                <p> this planet is not available</p>
+            }
+        </div>
+    )
 }
 
 export default Planets
